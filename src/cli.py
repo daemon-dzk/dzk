5c2676b0e5a2034dc0975a2197f3fc34b6213a9c
sc4r3p3r3 <12warbre@gmail.com>
2026-09-10 02:01:25 +0700

    dzk6

--- src/cli.py
+++ src/cli.py
@@ -6,13 +6,13 @@ dzk - Personal Security Utility Toolkit
 import sys
 import argparse
 from src.modules.analyze import analyze_file
-from modules.hash import hash_file
-from modules.metadata import get_metadata
-from modules.entropy import calculate_entropy
-from modules.strings import extract_strings
-from modules.dns import query_dns
-from modules.http import http_analyze
-from modules.pcap import pcap_analyze
+from src.modules.hash import hash_file
+from src.modules.metadata import get_metadata
+from src.modules.entropy import calculate_entropy
+from src.modules.strings import extract_strings
+from src.modules.dns import query_dns
+from src.modules.http import http_analyze
+from src.modules.pcap import pcap_analyze
 
 
 def main():
@@ -47,16 +47,16 @@ def main():
     strings_parser.add_argument("target", help="File to analyze")
     strings_parser.add_argument("--min-len", type=int, default=4, help="Minimum string length")
     
-    # NEW: Module: dns
+    # Module: dns
     dns_parser = subparsers.add_parser("dns", help="Query DNS records")
     dns_parser.add_argument("domain", help="Domain to query")
     dns_parser.add_argument("--type", "-t", default="A", choices=["A", "AAAA", "MX", "NS", "TXT", "CNAME"], help="Record type")
     
-    # NEW: Module: http
+    # Module: http
     http_parser = subparsers.add_parser("http", help="Analyze HTTP endpoint")
     http_parser.add_argument("url", help="URL to analyze")
     
-    # NEW: Module: pcap
+    # Module: pcap
     pcap_parser = subparsers.add_parser("pcap", help="Analyze PCAP file")
     pcap_parser.add_argument("file", help="PCAP file to analyze")
     
